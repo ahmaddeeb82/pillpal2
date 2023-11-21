d2c2a4a66affaa18050965393875d66d6288a909
Deeb <deebahmad2023@gmail.com>
2023-11-21 23:14:16 +0300

    Organized OrderController And Adding Some Erorr Statuses

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,65 +3,97 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponse;
+use App\Http\Resources\OrderMedicineResource;
+use App\Http\Resources\OrderResource;
 use App\Models\Medicine;
 use App\Models\MedicineOrder;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
+use function PHPUnit\Framework\isEmpty;
+
 class OrderController extends Controller
 {
-    public function createOrder(Request $request) {
-    $order_meds = $request->all();
-    if (!$order_meds) {
-        return ApiResponse::apiSendResponse(400, 'Some Order Details Are Missed.');
-    }
+    public function createOrder(Request $request)
+    {
 
-    $order = Order::create([
-        'user_id' => auth()->user()->id,
-        'order_date' => Carbon::now()->toDateString(),
-    ]);
+        $order_meds = $request->all();
 
-    $total_price = 0;
-
-    foreach ($order_meds as $order_med) {
-        $medicine = Medicine::where('id', $order_med['medicine_id'])->first();
-
-        if (!$medicine) {
-            return ApiResponse::apiSendResponse(400, 'Medicine with ID ' . $order_med['medicine_id'] . ' does not exist.');
+        if (!$order_meds) {
+            return ApiResponse::apiSendResponse(400, 'Some Order Data Are Missed.');
         }
 
-        $medicine_price = $medicine->price;
-        $quantity_price = $order_med['quantity'] * $medicine_price;
-        MedicineOrder::create([
-            'order_id' => $order->id,
-            'medicine_id' => $order_med['medicine_id'],
-            'quantity' => $order_med['quantity'],
-            'quantity_price' => $quantity_price,
+        $order = Order::create([
+            'user_id' => auth()->user()->id,
+            'order_date' => Carbon::now()->toDateString(),
         ]);
-        $total_price += $quantity_price;
+
+        $total_price = 0;
+        $error = false;
+
+        foreach ($order_meds as $order_med) {
+
+            if (!isset($order_med['medicine_id']) || !$order_med['quantity']) {
+                $error = true;
+                break;
+            }
+
+            $medicine = Medicine::where('id', $order_med['medicine_id'])->first();
+
+            if (!$medicine) {
+                return ApiResponse::apiSendResponse(400, 'Medicine with ID ' . $order_med['medicine_id'] . ' does not exist.');
+            }
+
+            $medicine_price = $medicine->price;
+
+            $quantity_price = $order_med['quantity'] * $medicine_price;
+
+            $addedMedicines[] = MedicineOrder::create([
+                'order_id' => $order->id,
+                'medicine_id' => $order_med['medicine_id'],
+                'quantity' => $order_med['quantity'],
+                'quantity_price' => $quantity_price,
+            ]);
+
+            $total_price += $quantity_price;
+        }
+
+        if ($error) {
+            foreach ($addedMedicines as $addedMedicine) {
+                $addedMedicine->delete();
+            }
+            return ApiResponse::apiSendResponse(400, 'Some Order Data Are Missed');
+        }
+
+        $order->update([
+            'total_price' => $total_price,
+        ]);
+
+        return ApiResponse::apiSendResponse(200, 'Order Has Been Added Successfully', OrderResource::collection(Order::where('user_id', auth()->user()->id)->get()));
     }
 
-    $order->update([
-        'total_price' => $total_price,
-    ]);
+    public function userOrders(Request $request)
+    {
+        $user_id = auth()->user()->id;
 
-    return ApiResponse::apiSendResponse(200, '', $order);
-}
+        $orders = Order::where('user_id', $user_id)->get();
 
-public function userOrders(Request $request) {
-    $user_id = auth()->user()->id;
-    $orders = Order::where('user_id', $user_id)->get();
-    return $orders;
-}
+        if(isEmpty($orders)) {
+            return ApiResponse::apiSendResponse(200, 'There Is No Order For This User.');
+        }
 
-public function orderDetails(Request $request) {
-    $order = Order::where('id', $request->order_id)->first();
-    if (!$order) {
-        return ApiResponse::apiSendResponse(400, 'Medicine with ID  does not exist.');
+        return ApiResponse::apiSendResponse(200, 'Orders Has Been Retrieved Successfully', OrderResource::collection($orders));
     }
-    $medicines = $order->medicines;
-    return response()->json($medicines);
-}
 
+    public function orderDetails(Request $request)
+    {
+        $order = Order::where('id', $request->order_id)->first();
+
+        if (!$order) {
+            return ApiResponse::apiSendResponse(400, 'Some Order Deatails Are Missed');
+        }
+
+        return ApiResponse::apiSendResponse(200, 'Orders Has Been Retrieved Successfully', new OrderMedicineResource($order));
+    }
 }
