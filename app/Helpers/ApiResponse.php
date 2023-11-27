4d8e33eafbca92cd38ca7150903cc26caa94f370
Deeb <deebahmad2023@gmail.com>
2023-11-27 17:33:27 +0300

    Edited The ApiRespnes Class To Have The Localization If Statuses

--- app/Helpers/ApiResponse.php
+++ app/Helpers/ApiResponse.php
@@ -1,13 +1,23 @@
 <?php
 
 namespace App\Helpers;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 class ApiResponse {
-    public static function apiSendResponse($code = 200, $message = null, $data = []) {
-        return response()->json([
-            "code"=> $code,
-            "message"=> $message,
-            "data"=> $data,
-        ],$code);
+    public static function apiSendResponse($code = 200, $message1 = null,$message2 = null, $data = []) {
+        if(LaravelLocalization::getCurrentLocale() == 'ar') {
+            return response()->json([
+                "code"=> $code,
+                "message"=> $message2,
+                "data"=> $data,
+            ],$code);
+        }
+        else {
+            return response()->json([
+                "code"=> $code,
+                "message"=> $message1,
+                "data"=> $data,
+            ],$code);
+        }
     }
 }
