aed54a247e6d70d83b61b22c97d4105b8c941cbe
SarahNabhan <sarahhefaa@gmail.com>
2023-12-24 21:59:59 +0300

    Created category controller (add+show+edit)

--- app/Http/Controllers/Dashboard/AdminCategoryController.php
+++ app/Http/Controllers/Dashboard/AdminCategoryController.php
@@ -2,12 +2,55 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class AdminCategoryController extends Controller
 {
-    public function addCategory(){
-        
+    public function addCategory(CategoryRequest $request){
+        $admin_id = auth()->guard('admin')->user()->id;
+        Category::create([
+            'name'=>[ 
+                'en' => $request->name,
+                'ar' => $request->name_ar],
+            'admin_id' => $admin_id,
+            'image' => $request->image,
+        ]);
+
+        return ApiResponse::apiSendResponse(
+            201,
+            'Category Has Been Created Successfully!',
+            'تمت إضافة التصنيف بنجاح'
+        );
     }
+
+    public function showCategories(){
+        $admin_id = auth()->guard('admin')->user()->id;
+        $categories = Category::where('admin_id',$admin_id)->get();
+        if(count($categories)==0){
+            return ApiResponse::apiSendResponse(
+                200,
+                'There are no categories',
+                'لا يوجد تصنيفات'
+            );
+        }
+
+        return ApiResponse::apiSendResponse(
+            200,
+            'Categories data has been retrieved successfully',
+            'تم اعادة التصنيفات بنجاح',
+            CategoryResource::collection($categories)
+        );        
+    }
+
+    public function editCategory(CategoryRequest $request){
+        $admin_id = auth()->guard('admin')->user()->id;
+        $categories = Category::where('admin_id', $admin_id)->get();
+        $category_id = $request->input('category_id');
+        
+        }
 }
