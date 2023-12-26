e7d056d45b4c2b354b3bbe9e44c555008a9e36db
SarahNabhan <sarahhefaa@gmail.com>
2023-12-26 12:07:29 +0300

    Added Medicine controller for add

---
+++ app/Http/Controllers/Dashboard/AdminMedicineController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Helpers\ApiResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\MedicineRequest;
+use App\Http\Resources\CompanyResource;
+use App\Models\Company;
+use App\Models\Medicine;
+use App\Models\MedicineCategory;
+use Illuminate\Http\Request;
+
+class AdminMedicineController extends Controller
+{
+    public function addMedicine(MedicineRequest $request){
+        $admin_id = auth()->guard('admin')->user()->id;
+        $image = $request->file('image');
+        $uploadFolder = 'medicines/'. auth()->guard('admin')->user()->id;
+        $imagePath = $image->store($uploadFolder, 'public');
+        $medicine = Medicine::create([
+            'scientific_name' => $request -> scientific_name,
+            'commercial_name' => $request -> commercial_name,
+            'quantity' => $request -> quantity,
+            'price' => $request -> price,
+            'expiration_date' => $request -> expiration_date,
+            'company_id' => $request -> company_id,
+            'admin_id' => $admin_id,
+            'image' => $imagePath
+        ]);
+        $categoy_id = $request -> input('category_id');
+        $medicine -> categories() -> attach($categoy_id);
+       
+        return ApiResponse::apiSendResponse(
+            201,
+            'Medicine Has Been Created Successfully!',
+            'تمت إضافة الدواء بنجاح'
+        );
+    }
+    
+
+    public function homMedicineInfo(){
+        
+    }
+}
