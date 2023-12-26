9ff088e25bb9a17a3e090a4b830f58dbaf0af7dd
SarahNabhan <sarahhefaa@gmail.com>
2023-12-26 12:57:38 +0300

    Edited on Medicine controller to add function for medicines home

--- app/Http/Controllers/Dashboard/AdminMedicineController.php
+++ app/Http/Controllers/Dashboard/AdminMedicineController.php
@@ -6,6 +6,7 @@ use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MedicineRequest;
 use App\Http\Resources\CompanyResource;
+use App\Http\Resources\MedicineHomeDashboardResource;
 use App\Models\Company;
 use App\Models\Medicine;
 use App\Models\MedicineCategory;
@@ -40,6 +41,21 @@ class AdminMedicineController extends Controller
     
 
     public function homMedicineInfo(){
-        
+        $admin_id = auth()->guard('admin')->user()->id;
+        $medicines = Medicine::where('admin_id',$admin_id)->get();
+        if(count($medicines)==0){
+            return ApiResponse::apiSendResponse(
+                200,
+                'There are no medicines',
+                'لا يوجد ادوية'
+            );
+        }
+
+        return ApiResponse::apiSendResponse(
+            200,
+            'Medicines data has been retrieved successfully',
+            'تم اعادة الادوية بنجاح',
+            MedicineHomeDashboardResource::collection($medicines)
+        );
     }
 }
